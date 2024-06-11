5ffec447fbc32a024451d87ba2585b8f5f31e61f
sargsyandavid7 <sargsyand648@gmail.com>
2024-06-11 14:47:53 +0400

    more structred project

--- srcs/pipex_bonus.c
+++ srcs/pipex_bonus.c
@@ -6,7 +6,7 @@
 /*   By: dasargsy <dasargsy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 18:14:41 by dasargsy          #+#    #+#             */
-/*   Updated: 2024/06/11 14:02:12 by dasargsy         ###   ########.fr       */
+/*   Updated: 2024/06/11 14:47:36 by dasargsy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,12 @@
 
 int	main(int argc, char **argv, char **envp)
 {
-	int		pid;
-	int		pid2;
 	int		i;
 	int		fds[2];
+	char	*data;
 
 	i = 2;
-	pid = 0;
-	pid2 = 0;
+	data = NULL;
 	if (argc < 5)
 	{
 		write(2, "Error: Not enough arguments\n", 28);
@@ -30,7 +28,12 @@ int	main(int argc, char **argv, char **envp)
 	}
 	while (argv[i + 1] != NULL)
 	{
-		fork_logic(fds, argv, i, envp);
+		logic(fds, argv, i, envp);
+		if (argv[i + 2] == NULL)
+		{
+			close(fds[1]);
+			break ;
+		}
 		i++;
 	}
 }
